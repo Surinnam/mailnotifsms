46d149ee40cf5125362d1bea201af1aa2a663dc1
Marius Michaud <marius.michaud@grenoble-inp.org>
2018-10-22 16:31:08 +0200

    Fetch and send subject

--- main.sh
+++ main.sh
@@ -1,3 +1,30 @@
+#! /bin/bash
 
-openssl s_client -crlf -connect webmail.grenoble-inp.org:993
-* 2033 FETCH BODY.PEEK[HEADER.FIELDS (SUBJECT)]
+
+if [ -f config ]
+then
+    source config
+else
+    echo "Create a config file with the following format:"
+    echo "LOGIN=\"login\""
+    echo "PASS=\"pass\""
+    echo "ID=12345678"
+    echo "APIKEY=\"xxxxxxxxxxxxx\""
+    echo "SERVER=\"server.domain\""
+    echo "PORT=123"
+fi
+
+fetch_msg() {
+    (echo ". LOGIN ""$LOGIN"" ""$PASS"; sleep 1;
+     echo ". SELECT INBOX"; sleep 1;
+     echo ". FETCH 2033 BODY.PEEK[HEADER.FIELDS (SUBJECT)]"; sleep 1;
+     ) | openssl s_client -crlf -connect "$SERVER":"$PORT" 2> /dev/null
+}
+
+
+OUT=$(fetch_msg | grep Subject)
+
+curl -G\
+    --data-urlencode "msg=New mail!""$OUT"\
+    "https://smsapi.free-mobile.fr/sendmsg?user=""$ID""&pass=""$APIKEY"
+
