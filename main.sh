4264e179fdae9ea2d1c126ddd1a66fde8deb9824
Marius Michaud <marius.michaud@grenoble-inp.org>
2018-10-25 10:04:02 +0200

    fetch lastmessage id if not set

--- main.sh
+++ main.sh
@@ -1,13 +1,5 @@
 #! /bin/bash
 
-if [ -f lastmessage ]
-then
-    source lastmessage
-else
-    echo "Last message unset"
-    exit 0
-fi
-
 if [ -f config ]
 then
     source config
@@ -29,15 +21,38 @@ fetch_msg() {
      ) | openssl s_client -crlf -connect "$SERVER":"$PORT" 2> /dev/null
 }
 
+fetch_lastmessage() {
+    (echo ". LOGIN ""$LOGIN"" ""$PASS"; sleep 1;
+     echo ". SELECT INBOX"; sleep 1;
+     echo ". LOGOUT"; sleep 1;
+    ) | openssl s_client -crlf -connect "$SERVER":"$PORT" 2> /dev/null
+}
+
+write_nextmessage() {
+   echo "NEXTMESSAGE=""$NEXTMESSAGE" > nextmessage 
+}
+
+if [ -f nextmessage ]
+then
+    source nextmessage
+else
+    echo "Next message was not set"
+    LASTMESSAGE=$(fetch_lastmessage | grep EXISTS | sed 's/\* \([^ ]*\).*/\1/')
+    NEXTMESSAGE=$((LASTMESSAGE+1))
+    write_nextmessage
+    exit 0
+fi
 
 OUT=$(fetch_msg | grep Subject)
 
 
-while read -r line; do
-    curl -G\
-	--data-urlencode "msg=New mail!""$line"\
-	"https://smsapi.free-mobile.fr/sendmsg?user=""$ID""&pass=""$APIKEY"
-    LASTMESSAGE=$((LASTMESSAGE+1))
-done <<< "$OUT"
+if [ ${#OUT} -gt 0 ]; then
+   while read -r line; do
+       curl -G\
+	    --data-urlencode "msg=New mail!""$line"\
+	    "https://smsapi.free-mobile.fr/sendmsg?user=""$ID""&pass=""$APIKEY"
+       NEXTMESSAGE=$((NEXTMESSAGE+1))
+   done <<< "$OUT"
+fi
 
-echo "LASTMESSAGE=""$LASTMESSAGE" > lastmessage
+write_nextmessage
